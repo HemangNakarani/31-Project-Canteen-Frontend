eb6be2b07ef9189c1d0aee544b9de6f598787a79
HemangNakarani <hemangnh18@gmail.com>
2021-04-26 00:47:30 +0530

    Refactored code

--- src/Pages/OwnerDashboard.js
+++ src/Pages/OwnerDashboard.js
@@ -1,29 +1,64 @@
-import React, { Component } from "react";
-import { Navbar, Container } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import PropTypes from "prop-types";
 import Dashboard from "../Components/Owners/Dashboard";
+import {
+  CssBaseline,
+  AppBar,
+  Toolbar,
+  Container,
+  useScrollTrigger,
+  Typography,
+  Box,
+} from "@material-ui/core";
 
-class OwnerDashboard extends Component {
-  state = {
-    canteenName: "PadmaKamal Caterers",
-    notifs: 10,
-  };
-  render() {
-    return (
-      <React.Fragment>
-        <Container fluid>
-          <Navbar expand="lg" variant="light" bg="light">
-            <Navbar.Brand>
-              <h1 style={{ fontFamily: "Garamond" }}>
-                <b>Welcome {this.state.canteenName} !!</b>
-              </h1>
-            </Navbar.Brand>
-          </Navbar>
-        </Container>
-        <Dashboard notifs={this.state.notifs} />
-      </React.Fragment>
-    );
-  }
+function ElevationScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+ElevationScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+function OwnerDashboard(props) {
+  // const [state, setState] = useState({
+  //   canteenName: "PadmaKamal Caterers",
+  //   notifs: 10,
+  // });
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <ElevationScroll {...props}>
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h6">PadmaKamal Canteen</Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <Toolbar />
+      <Container>
+        <Box>
+          <Dashboard notifs={10} />
+        </Box>
+      </Container>
+    </React.Fragment>
+  );
 }
 
 export default OwnerDashboard;
